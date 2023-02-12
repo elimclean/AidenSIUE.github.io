0d80a19e53a9933bd328928f0b2c3ad7d313c739
elimclean <84139150+elimclean@users.noreply.github.com>
2023-02-12 10:52:12 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -166,24 +166,14 @@ document.addEventListener('keydown', function(e) {
     rightPaddle.dy = paddleSpeed;
   }
 
-  // w key
-  if (e.which === 87) {
-    leftPaddle.dy = -paddleSpeed;
-  }
-  // a key
-  else if (e.which === 83) {
-    leftPaddle.dy = paddleSpeed;
-  }
+    leftPaddle.dy = ball.y;
+ 
 });
 
 // listen to keyboard events to stop the paddle if key is released
 document.addEventListener('keyup', function(e) {
   if (e.which === 38 || e.which === 40) {
     rightPaddle.dy = 0;
-  }
-
-  if (e.which === 83 || e.which === 87) {
-    leftPaddle.dy = 0;
   }
 });
 
